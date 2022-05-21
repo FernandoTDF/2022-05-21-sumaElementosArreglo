71acaea2a4ee40fdcf0b4b97547ac498d3b28f3b
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-05-21 19:12:12 -0300

    Mejora agregada

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 let tamanoArreglo: number = Number(prompt("Ingrese la cantidad de elementos del Arreglo"));
 let arregloNumeros:number[] = new Array(tamanoArreglo);
 let indice:number;
-let elementoActual:number;
 let sumaElementos:number = 0;
 
 for (indice=0; indice < tamanoArreglo; indice++){
   
-  elementoActual = Number(prompt(`Ingrese el valor del elemento actual ${indice} de ${tamanoArreglo}`));
-  sumaElementos += elementoActual;
-  console.log(`El valor del elemento ${indice} es ${elementoActual} y la suma acumulada es ${sumaElementos}`);
-
+  arregloNumeros[indice] = Number(prompt(`Ingrese el valor del elemento
+   actual ${indice} de ${tamanoArreglo}`));
+  sumaElementos += arregloNumeros[indice];
+  console.log(`El valor del elemento ${indice} es ${arregloNumeros[indice]} y
+   la suma acumulada es ${sumaElementos}`);
 }
